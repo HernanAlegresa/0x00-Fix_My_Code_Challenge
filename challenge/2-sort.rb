0f4c5f5da1520f3348a6033802ab9faa048530cc
HernanAlegresa <alegresah@gmail.com>
2024-09-15 19:00:06 -0300

    Rubyas o morochas?

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -20,7 +20,7 @@ ARGV.each do |arg|
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg) # inserting in position i instead of i - 1
             is_inserted = true
             break
         end
